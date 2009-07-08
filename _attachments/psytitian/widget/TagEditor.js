ceec95940163763a905af5161ddf626f553eec15
Robert Burrell Donkin <rdonkin@apache.org>
2009-07-08 09:23:59 +0100

    Outline TagEditor widget for fields composed of fields

---
+++ _attachments/psytitian/widget/TagEditor.js
@@ -0,0 +1,25 @@
+ /*   
+  * Copyright [2009] Robert Burrell Donkin http://robertburrelldonkin.name
+  * 
+  *  Licensed under the Apache License, Version 2.0 (the "License");
+  *  you may not use this file except in compliance with the License.
+  *  You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  *  Unless required by applicable law or agreed to in writing, software
+  *  distributed under the License is distributed on an "AS IS" BASIS,
+  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  *  See the License for the specific language governing permissions and
+  * limitations under the License. 
+  */
+dojo.provide("psytitian.widget.TagEditor");
+dojo.require("dijit._Widget");
+dojo.require("dijit._Templated");
+
+dojo.declare("psytitian.widget.TagEditor",
+[dijit._Widget, dijit._Templated], {
+    templateString:null,
+    templatePath: dojo.moduleUrl("psytitian", "widget/TagEditor.html"),
+    widgetsInTemplate:true
+});
