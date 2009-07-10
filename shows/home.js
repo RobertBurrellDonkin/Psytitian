a294a68dee9643fedf52e2c34b5260e4a0c304f4
Robert Burrell Donkin <rdonkin@apache.org>
2009-07-10 10:59:26 +0100

    Add in data from widget

--- shows/home.js
+++ shows/home.js
@@ -1,8 +1,10 @@
 function(doc, req) {
   if(doc) {
 	  var style = '<style type="text/css">@import "/citation/_design/cite/dijit/themes/tundra/tundra.css";@import "/citation/_design/cite/dojo/resources/dojo.css";@import "/citation/_design/cite/style/main.css";</style>';
-	  var head = '<head><title>' + doc._id + '</title>' + style + '</head>';
-	  var body = '<body class="tundra"><h1>' + doc._id + '</h1>';
+	  var dojo = '<script type="text/javascript" src="/citation/_design/cite/dojo/dojo.js" djConfig="parseOnLoad:true, isDebug:true"></script>';
+	  var preamble = '<script type="text/javascript">dojo.require("dojo.parser");dojo.require("psytitian.widget.Thing");</script>';
+	  var head = '<head><title>' + doc._id + '</title>' + style + dojo + preamble + '</head>';
+	  var body = '<body class="tundra"><div dojoType="psytitian.widget.Thing" url="' + doc._id + '"></div><h1>' + doc._id + '</h1>';
 	  
 	  if (doc.created) {
 		  body = body + "<p>Loaded on " + doc.created + "</p>";
