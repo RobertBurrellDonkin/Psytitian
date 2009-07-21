2bf8fd59a89c1bb9635fe324df03663400b6f3b9
Robert Burrell Donkin <rdonkin@apache.org>
2009-07-21 11:14:06 +0100

    Added GPL3 headers

--- shows/home.js
+++ shows/home.js
@@ -1,3 +1,21 @@
+ /*   
+  * Copyright [2009] Robert Burrell Donkin http://robertburrelldonkin.name
+  * 
+  *  This file is part of Psytitian.
+  *
+  *  Psytitian is free software: you can redistribute it and/or modify
+  *  it under the terms of the GNU General Public License as published by
+  *  the Free Software Foundation, either version 3 of the License, or
+  *  (at your option) any later version.
+  *
+  *  Psytitian is distributed in the hope that it will be useful,
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *  GNU General Public License for more details.
+  *
+  *  You should have received a copy of the GNU General Public License
+  *  along with Psytitian.  If not, see <http://www.gnu.org/licenses/>.
+  */
 function(doc, req) {
   if(doc) {
 	  var style = '<style type="text/css">@import "/citation/_design/cite/dijit/themes/tundra/tundra.css";@import "/citation/_design/cite/dojo/resources/dojo.css";@import "/citation/_design/cite/style/main.css";@import "/citation/_design/cite/psytitian/resources/psytitian.css";</style>';
