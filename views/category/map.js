da403a86dfeafa7638aebc1c30119e1aa3fc732a
Robert Burrell Donkin <rdonkin@apache.org>
2010-02-09 21:59:41 +0000

    Prepare to add category to journal entries

---
+++ views/category/map.js
@@ -0,0 +1,33 @@
+ /*   
+  * Copyright [2010] Robert Burrell Donkin http://robertburrelldonkin.name
+  * 
+  *  This file is part of Psytitian.
+  *
+  *  Psytitian is free software: you can redistribute it and/or modify
+  *  it under the terms of the GNU General Public License as published by
+  *  the Free Software Foundation, either version 3 of the License, or
+  *  (at your option) any later version.
+  *
+  *  Psytitian is distributed in the hope that it will be useful,
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *  GNU General Public License for more details.
+  *
+  *  You should have received a copy of the GNU General Public License
+  *  along with Psytitian.  If not, see <http://www.gnu.org/licenses/>.
+  */
+function(doc) {
+	for (type in doc.types) {
+		if (doc.types[type] == "http://www.w3.org/2005/Atom#category") {
+			var isCategory = true;
+		}
+	}
+
+	if (isCategory) {
+		var title = doc._id;
+		if (doc.title) {
+			title = doc.title;
+		}
+		emit(doc._id, title);
+	}
+};
